067e9e65ca87961df7f3b009aa5927c715569f1c
noman-swe <noman.swe130@gmail.com>
2022-07-22 00:17:29 +0600

    Math.functions

---
+++ math.js
@@ -0,0 +1,24 @@
+const myNum = -5;
+const output = Math.abs(myNum);
+console.log(output);
+
+// Math.ceil -takes the higher number
+const myNumber2 = 14.4534;
+const ceilOutput = Math.ceil(myNumber2);
+console.log(ceilOutput);
+
+// Math.floor -takes the lower number
+const myNumber3 = 65.35;
+const floorOutput = Math.floor(myNumber3);
+// round
+const roundOutput = Math.round(myNumber3);
+// random
+const random = Math.random() * 10;
+
+const randomRound = Math.round(random);
+
+console.log(floorOutput);
+console.log(roundOutput);
+console.log(random);
+console.log(randomRound);
+// 
