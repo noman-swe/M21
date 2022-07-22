169e5bfa5b82fb8bab3b468b7c2043f9d68fa608
noman-swe <noman.swe130@gmail.com>
2022-07-22 10:43:41 +0600

    21-5 Find the largest element of an array

---
+++ largest.js
@@ -0,0 +1,16 @@
+const numbers = [44, 23, 34, 32, 54, 5, 78];
+
+function largestElement(numbers) {
+    let largest = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element > largest) {
+            largest = element;
+        }
+    }
+    return largest;
+}
+const largeValue = largestElement(numbers);
+console.log(largeValue);
+
+
