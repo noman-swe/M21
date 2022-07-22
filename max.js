2b79788c5d88f7dfb7e73409cbfd211dbd8653e6
noman-swe <noman.swe130@gmail.com>
2022-07-22 10:08:39 +0600

    21-3 Find max of two values, find max of three values

---
+++ max.js
@@ -0,0 +1,40 @@
+const business = 500;
+const minister = 656;
+const army = 784;
+
+const largerNum = Math.max(business, minister, army);
+console.log(largerNum);
+
+// by function
+function largestNum (first, second){
+    if(first > second){
+        return first;
+    }
+    else{
+        return second;
+    }
+}
+
+const large = largestNum(55, 52);
+console.log('large number is: ', large);
+
+
+var equal ='All are equal.';
+// conpare between 3 numbers
+function largerBetweenThreeNums( first, second, third){
+    if(first > second && first > third){
+        return first;
+    }
+    else if(second > first && second > third){
+        return second;
+    }
+    else if(first === second && first === third){
+        return equal;
+    }
+    else {
+        return third;
+    }
+}
+
+const largerNumber = largerBetweenThreeNums(10, 10, 10);
+console.log(largerNumber);
